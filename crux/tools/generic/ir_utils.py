48635b6e0a25b4862277153468daa7cabda6253a
DylanJu <jju8@jh.edu>
2025-10-20 18:07:18 -0400

    temporaraily update, pilot test of evaluation

--- crux/tools/generic/ir_utils.py
+++ crux/tools/generic/ir_utils.py
@@ -30,7 +30,7 @@ def load_run_or_qrel(path, topk=1000, threshold=1):
 def load_diversity_qrel(path):
     import pandas as pd
     df = pd.read_csv(path, sep='\s+', names=['query_id', 'iteration', 'doc_id', 'relevance'])
-    print(df)
+    # print(df)
     return df
     # import ir_measures
     # return ir_measures.read_trec_qrels(path)
@@ -109,33 +109,3 @@ def binarize(qrels):
 #         searcher = LuceneSearcher(path)
 #         searcher.set_bm25(k1=0.9, b=0.4)
 #     return searcher
-
-# def load_runs(path, topk=None, output_score=False): # support .trec file only
-#     run_dict = defaultdict(list)
-#     with open(path, 'r') as f:
-#         for line in f:
-#             qid, _, docid, rank, score, _ = line.strip().split()
-#             if int(rank) <= (9999 or topk):
-#                 run_dict[str(qid)] += [(docid, float(rank), float(score))]
-#
-#     # sort by score and return static dictionary
-#     sorted_run_dict = OrderedDict()
-#     for qid, docid_ranks in run_dict.items():
-#         sorted_docid_ranks = sorted(docid_ranks, key=lambda x: x[1], reverse=False) 
-#         if output_score:
-#             # sorted_run_dict[qid] = [{docid, rel_score} for docid, rel_rank, rel_score in sorted_docid_ranks]
-#             sorted_run_dict[qid] = {docid: rel_score for docid, rel_rank, rel_score in sorted_docid_ranks}
-#         else:
-#             sorted_run_dict[qid] = [docid for docid, _, _ in sorted_docid_ranks]
-#
-#     return sorted_run_dict
-
-# def batch_iterator(iterable, size=1, return_index=False):
-#     l = len(iterable)
-#     for ndx in range(0, l, size):
-#         if return_index:
-#             yield (ndx, min(ndx + size, l))
-#         else:
-#             yield iterable[ndx:min(ndx + size, l)]
-
-
